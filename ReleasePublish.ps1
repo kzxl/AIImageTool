3e5389b0e23e516029f98aec99843cbf070075cf
Phong Võ <phong.vo@mylangroup.com>
2026-04-09 21:17:07 +0700

    Fix Native DLL loading issue in Lite and Dev environments by removing recursive duplicate runtimes from output and enforcing win-x64 RID

--- ReleasePublish.ps1
+++ ReleasePublish.ps1
@@ -10,16 +10,15 @@ if (Test-Path "Publish_Release") {
 Write-Host "Publishing FULL (Self-Contained)..."
 $solDir = (Get-Item .).FullName + "\"
 
+dotnet build ImageTool.Plugins.FaceRestorer\ImageTool.Plugins.FaceRestorer.csproj -c Release -p:SolutionDir=$solDir
+dotnet build ImageTool.Plugins.Upscaler\ImageTool.Plugins.Upscaler.csproj -c Release -p:SolutionDir=$solDir
+
 dotnet publish ImageTool.Host\ImageTool.Host.csproj -c Release -r win-x64 -p:SelfContained=true -p:PublishSingleFile=true -p:SolutionDir=$solDir -o "Publish_Release\Full"
-dotnet publish ImageTool.Worker.Upscaler\ImageTool.Worker.Upscaler.csproj -c Release -r win-x64 -p:SelfContained=true -p:PublishSingleFile=true -p:SolutionDir=$solDir -o "Publish_Release\Full"
-dotnet publish ImageTool.Plugins.FaceRestorer\ImageTool.Plugins.FaceRestorer.csproj -c Release -p:SolutionDir=$solDir -o "Publish_Release\Full\Plugins\ImageTool.Plugins.FaceRestorer"
-dotnet publish ImageTool.Plugins.Upscaler\ImageTool.Plugins.Upscaler.csproj -c Release -p:SolutionDir=$solDir -o "Publish_Release\Full\Plugins\ImageTool.Plugins.Upscaler"
+Copy-Item -Path "ImageTool.Host\bin\Release\net8.0-windows\win-x64\Plugins" -Destination "Publish_Release\Full\Plugins" -Recurse -Force
 
 Write-Host "Publishing LITE (Framework-Dependent)..."
 dotnet publish ImageTool.Host\ImageTool.Host.csproj -c Release -r win-x64 -p:SelfContained=false -p:PublishSingleFile=true -p:SolutionDir=$solDir -o "Publish_Release\Lite"
-dotnet publish ImageTool.Worker.Upscaler\ImageTool.Worker.Upscaler.csproj -c Release -r win-x64 -p:SelfContained=false -p:PublishSingleFile=true -p:SolutionDir=$solDir -o "Publish_Release\Lite"
-dotnet publish ImageTool.Plugins.FaceRestorer\ImageTool.Plugins.FaceRestorer.csproj -c Release -p:SolutionDir=$solDir -o "Publish_Release\Lite\Plugins\ImageTool.Plugins.FaceRestorer"
-dotnet publish ImageTool.Plugins.Upscaler\ImageTool.Plugins.Upscaler.csproj -c Release -p:SolutionDir=$solDir -o "Publish_Release\Lite\Plugins\ImageTool.Plugins.Upscaler"
+Copy-Item -Path "ImageTool.Host\bin\Release\net8.0-windows\win-x64\Plugins" -Destination "Publish_Release\Lite\Plugins" -Recurse -Force
 
 Write-Host "Compressing ZIP packages..."
 Compress-Archive -Path "Publish_Release\Full\*" -DestinationPath "Publish_Release\ImageTool_Full_Win_x64.zip" -Force
