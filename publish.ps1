b99a3dd499da949a55712a7ca21678e70dac7c0d
Phong Võ <phong.vo@mylangroup.com>
2026-04-21 08:29:01 +0700

    fix: correct parameter for self-contained in publish shell

--- publish.ps1
+++ publish.ps1
@@ -9,25 +9,25 @@ $pluginsDir = @(
 
 # Lite Build
 Write-Host "Publishing Host Lite..."
-dotnet publish $hostProj -c Release -r win-x64 --self-contained false -p:PublishSingleFile=true -o "$slnDir\Publish\Lite"
+dotnet publish $hostProj -c Release -r win-x64 -p:SelfContained=false -p:PublishSingleFile=true -o "$slnDir\Publish\Lite"
 
 foreach ($plugin in $pluginsDir) {
     if (Test-Path $plugin) {
         $pluginName = (Get-Item $plugin).BaseName
         Write-Host "Publishing $pluginName Lite..."
-        dotnet publish $plugin -c Release -r win-x64 --self-contained false -o "$slnDir\Publish\Lite\Plugins\$pluginName"
+        dotnet publish $plugin -c Release -r win-x64 -p:SelfContained=false -o "$slnDir\Publish\Lite\Plugins\$pluginName"
     }
 }
 
 # Full Build
 Write-Host "Publishing Host Full..."
-dotnet publish $hostProj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o "$slnDir\Publish\Full"
+dotnet publish $hostProj -c Release -r win-x64 -p:SelfContained=true -p:PublishSingleFile=true -o "$slnDir\Publish\Full"
 
 foreach ($plugin in $pluginsDir) {
     if (Test-Path $plugin) {
         $pluginName = (Get-Item $plugin).BaseName
         Write-Host "Publishing $pluginName Full..."
-        dotnet publish $plugin -c Release -r win-x64 --self-contained false -o "$slnDir\Publish\Full\Plugins\$pluginName"
+        dotnet publish $plugin -c Release -r win-x64 -p:SelfContained=false -o "$slnDir\Publish\Full\Plugins\$pluginName"
     }
 }
 
